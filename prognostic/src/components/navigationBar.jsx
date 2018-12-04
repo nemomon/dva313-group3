623a6f82f0bfdf3a9cae7b8d157b62b3c1e65001
Erika W <erikaweilander@hotmail.co.ukls>
2018-12-04 13:44:34 +0100

    playing with nav-bar

--- prognostic/src/components/navigationBar.jsx
+++ prognostic/src/components/navigationBar.jsx
@@ -1,14 +1,71 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Navbar, NavbarBrand, NavbarNav, NavItem, NavLink, NavbarToggler, Collapse, FormInline, Dropdown, DropdownToggle, DropdownMenu,  DropdownItem } from "mdbreact";
 
-class NavigationBar extends Component {
+
+
+class NavigationBar extends React.Component {
   state = {
-    /* The state of the class goes here
-         To update the state, call setState() */
+    isOpen: false
   };
 
+  toggleCollapse = this.setState({ isOpen: !this.state.isOpen });
+
   render() {
-    return <div>This is a really pretty navbar</div>;
+    return (
+
+      <Navbar color="indigo" dark expand="md">
+          <NavbarBrand>
+            <strong className="white-text">Navbar</strong>
+          </NavbarBrand>
+          <NavbarToggler
+            onClick={this.toggleCollapse}
+          />
+          <Collapse
+            id="navbarCollapse3"
+            isOpen={this.state.isOpen}
+            navbar
+          >
+            <NavbarNav left>
+              <NavItem active>
+                <NavLink to="#!">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="#!">Features</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="#!">Pricing</NavLink>
+              </NavItem>
+              <NavItem>
+                <Dropdown>
+                  <DropdownToggle nav caret>
+                    <div className="d-none d-md-inline">Dropdown</div>
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem href="#!">Action</DropdownItem>
+                    <DropdownItem href="#!">Another Action</DropdownItem>
+                    <DropdownItem href="#!">Something else here</DropdownItem>
+                    <DropdownItem href="#!">Something else here</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </NavItem>
+            </NavbarNav>
+            <NavbarNav right>
+              <NavItem>
+                <FormInline waves>
+                  <div className="md-form my-0">
+                    <input
+                      className="form-control mr-sm-2"
+                      type="text"
+                      placeholder="Search"
+                      aria-label="Search"
+                    />
+                  </div>
+                </FormInline>
+              </NavItem>
+            </NavbarNav>
+          </Collapse>
+      </Navbar>
+    );
   }
 }
 /* Need to type this after creating a component so other files can import it */
