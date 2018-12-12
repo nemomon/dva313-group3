6d0d1b002552bc24718dd798c08eb435ac98b65a
nemomon <44428484+nemomon@users.noreply.github.com>
2018-12-12 20:32:22 +0100

    Updated navbar

--- prognostic/src/App.js
+++ prognostic/src/App.js
@@ -2,17 +2,25 @@ import React, { Component } from "react";
 import "./App.css";
 import NavigationBar from "./components/navigationBar";
 import AllocationTab from "./components/allocation_tab/allocationTab";
-import PersonTable from "./components/personsTable";
-import TestTable from "./components/testTable";
+import PersonTab from "./components/personTab";
+import ProjectTab from "./components/projectTab";
+import SpendingTab from "./components/spendingTab";
+import RemainingTab from "./components/remainingTab";
+import EndBalanceTab from "./components/endBalanceTab";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       /*  Attach the diffenrent components her to build the whole UI */
-      /* Replace <AllocationTab /> with <TestTable /> to try out the table */
       <React.Fragment>
         <NavigationBar />
-        <AllocationTab />
+        <Route path="/allocationTable" component={AllocationTab} />
+        <Route path="/personTable" component={PersonTab} />
+        <Route path="/projectTable" component={ProjectTab} />
+        <Route path="/spendingTable" component={SpendingTab} />
+        <Route path="/remaningTable" component={RemainingTab} />
+        <Route path="/endBalanceTable" component={EndBalanceTab} />
       </React.Fragment>
     );
   }
