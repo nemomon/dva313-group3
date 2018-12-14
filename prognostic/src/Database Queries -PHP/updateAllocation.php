6fa6f2c615b226f8416e189ea0da71937bdc1896
Samaan95 <Samaan95@example.com>
2018-12-14 19:00:13 +0100

    Added

---
+++ prognostic/src/Database Queries -PHP/updateAllocation.php
@@ -0,0 +1,18 @@
+<?php  
+
+session_start();
+ include_once 'connect/dbh.inc.php'; // connect to database  
+
+
+
+$query = "UPDATE Allocation SET  Percentage = ".$_POST['Percentage'].", StartDate= '".$_POST['StartDate']."', EndDate = ".$_POST['EndDate']." WHERE Id=".$_POST['Id'];
+
+if ($connect->query($query) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $connect->error;
+}
+
+
+
+?>
