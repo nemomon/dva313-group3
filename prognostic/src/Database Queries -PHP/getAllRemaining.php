72d6c6f4bef2525f7e632befd47e8e26aedb1a1d
Mohammed Abusamaan <45001859+Samaan95@users.noreply.github.com>
2018-12-18 20:24:22 +0100

    Update getAllRemaining.php

--- prognostic/src/Database Queries -PHP/getAllRemaining.php
+++ prognostic/src/Database Queries -PHP/getAllRemaining.php
@@ -14,7 +14,7 @@
                 //push all the rows in array
                 while($row = mysqli_fetch_array($result))  
                 {  
-                  $output[] = array_push($output, array(
+                    array_push($output, array(
                    'Id' => $row['Id'],
                    'Name' => $row['Name'],
                    'SpendingDate' => $row['SpendingDate'],
